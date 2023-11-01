dfd121f7a3b3cf37dc8b5965a2dac3b2e537153b
Taran <taran@Tarans-MacBook-Air.local>
2023-11-01 17:56:53 +0530

    update

---
+++ stocks.js
@@ -0,0 +1,17 @@
+const stockSymbols = [
+  'AAPL', 'GOOGL', 'AMZN', 'TCS.BSE', 'HCLTECH.BSE',
+  'WFC', // Wells Fargo
+  'JPM', // JPMorgan Chase & Co.
+  'FE', // FirstEnergy Corp.
+  'GDS', // GDS Holdings Limited
+  'PGRE', // Paramount Group, Inc.
+  'MSFT', // Microsoft Corporation
+  'NVDA', // NVIDIA Corporation
+  'LLY', // Eli Lilly and Company
+  'NFLX', // Netflix, Inc.
+  'INTC', // Intel Corporation
+  'LI' // Li Auto Inc.
+];
+
+module.exports = { stockSymbols };
+
