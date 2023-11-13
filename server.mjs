a330fee9851c944511e8ac454facdcdd83c72411
mastinder singh <mastinder@gmail.com>
2023-11-13 14:35:44 +0530

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,7 +1,7 @@
 const apm = require('elastic-apm-node').start({
   serviceName: 'stock-adviser',
-  secretToken: 'I5MWMcrB2iTyk3y8Vz',
-  serverUrl: 'https://bb90799eb81c43bba2ab8c4ebb66f486.apm.asia-south1.gcp.elastic-cloud.com:443',
+  secretToken: 'C8G6rOEdbbUZRJnrud,
+  serverUrl: ''https://4769643cf26448ed8e6887b301a5ddd9.apm.us-central1.gcp.cloud.es.io:443
   environment: 'production',
   logLevel: 'debug'
 });
